15ad1bcc08a1d8014e8ba2bdce8346c1335a9cd9
Niodor <czermar@gmail.com>
2021-05-31 22:29:39 +0200

    First upload

---
+++ GPT3WebSummarizer-main/websum/websum/Gpt3Stuff.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using OpenAI_API;
+
+namespace websum {
+    public class Gpt3Stuff {
+
+        public Gpt3Stuff() {
+            
+        }
+
+        public OpenAIAPI GetApi() {
+            // put your key in a text file "C:\Users\pengo\.openai" or "~/.openai"
+            string file = @".openai";
+            var apikeypath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), file);
+            string apikey = null;
+            try {
+                apikey = File.ReadAllText(apikeypath).Trim();
+            } catch (Exception e) {
+                Console.WriteLine("Couldn't read auth; error: " + e.Message);
+                Console.WriteLine($"Make a file containing api key at: {apikeypath}");
+            }
+            //var auth = APIAuthentication.LoadFromPath() // couldn't get this to work idk
+            var api = new OpenAIAPI(apiKeys: apikey, engine: Engine.Davinci);
+            return api;
+        }
+
+        public async Task<string> Run(string text = "One Two Three One Two") {
+
+            // https://github.com/OkGoDoIt/OpenAI-API-dotnet
+
+            var api = GetApi();
+
+            var req = new CompletionRequest(text, temperature: 0.5, max_tokens: 10);
+
+            // TODO: log/cache req
+
+            var result1 = await api.Completions.CreateCompletionAsync(req);
+
+            return result1.ToString(); // e.g. should be something starting with "Three"
+        }
+    }
+}
---
+++ GPT3WebSummarizer-main/websum/websum/WebSummaryController.cs
@@ -0,0 +1,56 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace websum {
+    class WebSummaryController : WebApiController {
+
+        // found at /api/time
+        [Route(HttpVerbs.Get, "/time")]
+        public string TimeNow() {
+            return "Zulu time is " + DateTime.UtcNow.ToString();
+        }
+
+        [Route(HttpVerbs.Get, "/sum/{word}")]
+        public string Sum(string word) {
+            return $"Summary of {word}: ";
+        }
+
+        // found at /api/gpt/mitochondria
+        [Route(HttpVerbs.Get, "/gpt/{word}")]
+        public async Task<string[]> Gpt(string word) {
+            var result = await new Gpt3Stuff().Run(word);
+            //return $"Gpt completiton of {word}: {result}";
+            return new string[] { $"{word}{result}" }; // json-ish
+            //await HttpContext.SendStringAsync($"{word}{result}", "text/plain", Encoding.UTF8);
+        }
+
+
+        // TODO: fix crash
+        // show a web page with the response
+        // found at /api/page/synchrotron
+        [Route(HttpVerbs.Get, "/page/{word}")]
+        public async void SumPage(string word) {
+
+            var result = await new Gpt3Stuff().Run(word);
+            var webpage = new WebTemplater().GetPage("answer", "{word}", $"this is a summary of {word}:<br/>{result}"); ;
+
+            //error: Cannot access a disposed object
+            await HttpContext.SendStringAsync(webpage, "text/html", Encoding.UTF8);
+        }
+
+
+        // show a web page with a test response
+        // found at /api/testpage/synchrotron
+        [Route(HttpVerbs.Get, "/testpage/{word}")]
+        public async void TestPage(string word) {
+            var webpage = new WebTemplater().GetPage("test page", "Summary", $"this is a test page about {word}"); ;
+            await HttpContext.SendStringAsync(webpage, "text/html", Encoding.UTF8);
+        }
+    }
+}
